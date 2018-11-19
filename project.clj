d843d3ff28122f63eca4fc430e2bf6bf25244913
Bob Beaty <drbob@themanfromspud.com>
2018-11-19 06:36:20 -0600

    Had to move to 1.7 for tests
    We were getting some nasty issue with the dependencies until we moved to
    1.7 - as I'm betting the updates to nippy, and byte-streams are
    expecting something in the 1.7-range. This now works just fine.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[com.taoensso/nippy "2.14.0"]
                  [primitive-math "0.1.6"]
                  [byte-streams "0.2.4"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [criterium "0.4.3"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :global-vars {*warn-on-reflection* true}
